4cf26cbaa31c74d2ff42cf045d02777eebe3d9a1
Marielle Porcidonio <mariporcidonio@hotmail.com>
2020-08-19 21:34:13 -0300

    Incluído estilo PageDefault

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function CadastroVideo() {
   return (
     <PageDefault>
-      Página de cadastro de vídeo
+      <h1>Cadastro de vídeo</h1>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
     </PageDefault>
   )
 }
